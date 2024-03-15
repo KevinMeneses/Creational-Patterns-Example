442ad1d9c0a38ef905cb1bb0db8c251123a4f719
Kevin Meneses <kevmeneses@gmail.com>
2024-03-14 23:34:09 -0500

    refactor

--- src/main/java/com/meneses/legacy/CameraCommandResult.java
+++ src/main/java/com/meneses/legacy/CameraCommandResult.java
@@ -6,44 +6,4 @@ public class CameraCommandResult {
     String type;
     String parameter;
     String listing;
-
-    public int getCommandCode() {
-        return commandCode;
-    }
-
-    public void setCommandCode(int commandCode) {
-        this.commandCode = commandCode;
-    }
-
-    public int getResultCode() {
-        return resultCode;
-    }
-
-    public void setResultCode(int resultCode) {
-        this.resultCode = resultCode;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getParameter() {
-        return parameter;
-    }
-
-    public void setParameter(String parameter) {
-        this.parameter = parameter;
-    }
-
-    public String getListing() {
-        return listing;
-    }
-
-    public void setListing(String listing) {
-        this.listing = listing;
-    }
 }
